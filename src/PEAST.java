0e51d411e572ac7ab9d126be4a6c2b193e8247c3
PNDaniel <daniel_nunes_21@hotmail.com>
2017-05-23 18:24:13 +0100

    GHCM started, 50% done

--- src/PEAST.java
+++ src/PEAST.java
@@ -41,41 +41,63 @@ public class PEAST {
 
         double current_temperature = 1/Math.log(Math.pow(0.75,-1)); // T0, initial temperature
         int k = current_round/2;
-        Matchup object_to_move = randomElement(split); // Set objectToMove = RandomObject(S)
+        Matchup object_to_move = randomObject(split); // Set objectToMove = RandomObject(S)
+        Slot target_cell = randomCell(split);
         int index = 0;
-        int last_increment = Integer.MAX_VALUE; // Supposedly it should be an infinite value, but "infinite" only exists
-                                                // with double types, so we used Integer.MAX_VALUE to simulate it with an int
+        double last_increment = Double.POSITIVE_INFINITY;
+
+        double costDiff = 0.0;
+
+        //TODO eu acho que o algoritmo ainda não faz nada, pois acho que não está a alterar custos
         while(index < maximum_sequence_length){
             double currentCost = split.getScheduleValue(); // currentCost of the iteration is the full cost(value) of a split
             if (index > 0){
                 // Inner annealing #1
-                current_temperature = getTemperature(current_temperature,maximum_sequence_length);
                 if(Math.random() < Math.exp(-1/current_temperature)) {
-                    //object_to_move =
-                    // TODO Set objectToMove = RandomObject(S,targetCell)
+                    object_to_move = moveToCell(object_to_move, target_cell); // Set targetCell = RandomCell(S)
                 }
                 else{
+                    //TODO Set objectToMove = LeastFitObject(S,targetCell)
+                }
+            }
 
+            // Inner annealing #2
+            if(Math.random() < current_temperature) {
+                target_cell = randomCell(split); // Set targetCell = RandomObject(S,targetCell)
+            }
+            else {
+                //TODO Set targetCell = FittestCell(S,objectToMove)
+            }
+            target_cell.AssignMatchup(object_to_move); // Move objectToMove to targetCell
+            if (split.getScheduleValue() > currentCost)
+            {
+                costDiff = split.getScheduleValue() - currentCost;
+                if (costDiff > last_increment){
+                    index = Integer.MAX_VALUE;
+                }
+                else{
+                    last_increment = costDiff;
                 }
             }
             current_temperature = getTemperature(current_temperature,maximum_sequence_length);
-            // Inner annealing #2
-            if(Math.random() < current_temperature) {
-
-            }
-            else {
-
-            }
             index++;
         }
         // Outer annealing
-        if( index == Integer.MAX_VALUE)
-        {
-            current_temperature = getTemperature(current_temperature,maximum_sequence_length);
-          //  if(Math.random() < Math.exp(-costDiff/current_temperature)) {
+        if( index == Integer.MAX_VALUE) {
+            if (Math.random() < Math.exp(-costDiff / current_temperature)) {
                 return split;
+            }
+        }
+        else{
+            //TODO Roll S back to the optimal point in the move sequence.
         }
         return split;
+    }
+
+    public Matchup moveToCell(Matchup matchup, Slot slot){
+
+        slot.AssignMatchup(matchup);
+        return slot.getMatch_assigned();
     }
 
     public double getTemperature(double T0, int m){
@@ -88,9 +110,15 @@ public class PEAST {
      * @param schedule
      * @return Matchup currently selected from the split, it will become the matchup
      */
-    private Matchup randomElement(Schedule schedule)
+    private Matchup randomObject(Schedule schedule)
     {
         int index = (int)(Math.random() * schedule.getSlots().size());
         return schedule.getSlots().get(index).getMatch_assigned();
+    }
+
+    private Slot randomCell(Schedule schedule)
+    {
+        int index = (int)(Math.random() * schedule.getSlots().size());
+        return schedule.getSlots().get(index);
     }
 }
